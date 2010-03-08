856d8f61168ec190f4de5f1e5508c54542067cc3
jgore <jgore@jgore.rdu.lulu.com>
2010-03-08 17:14:52 -0500

    Place license notice on our source files

--- publish/client/client.py
+++ publish/client/client.py
@@ -1,29 +1,14 @@
 #!/usr/bin/python
-
 """
 Client library for the Lulu Publish API
 
-Copyright (c) 2009 Lulu.com
+Copyright 2010 Lulu Enterprises
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+    http://www.apache.org/licenses/LICENSE-2.0
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-
-Michael DeHaan <mdehaan@lulu.com>
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License. 
 """
 
 import urllib2
@@ -224,7 +209,7 @@ class Client:
             self.__convert_error_to_exception(he)
         print simplejson.loads(response)
         return simplejson.loads(response)
-  
+
     def test_echo(self, parameters=None, form_data=None):
         """
         A simple echo method for unittest purposes.
@@ -240,7 +225,7 @@ class Client:
        as what would happen if input to a request was invalid.
        """
        return self.__submit("test_error1")
-  
+
     def __submit(self, method, options=None, form_data=None, download=None):
         """
         Carries out a request to the REST endpoint
